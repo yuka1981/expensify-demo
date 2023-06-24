60a6e4bb67cbab9f4aa612f126507f80d2a5d4fb
Reid <touyalin@gmail.com>
2023-06-24 16:26:53 +0800

    9.79 linking between routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/style.scss";
 
@@ -12,10 +12,34 @@ const EditExpensePage = () => <div>This is edit expense page.</div>;
 
 const HelpPage = () => <div>This is help page.</div>;
 
-const NotFoundPage = () => <div>404!!</div>;
+const NotFoundPage = () => (
+  <div>
+    404!!
+    <Link to="/">Go Home</Link>
+  </div>
+);
+
+const Header = () => (
+  <div>
+    <h1>Expencify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>
+      Dashboard
+    </NavLink>
+    <NavLink to="/create" activeClassName="is-active">
+      Create Expense
+    </NavLink>
+    <NavLink to="/edit" activeClassName="is-active">
+      Edit Expense
+    </NavLink>
+    <NavLink to="/help" activeClassName="is-active">
+      Help
+    </NavLink>
+  </div>
+);
 
 const routes = (
   <BrowserRouter>
+    <Header />
     <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage} />
