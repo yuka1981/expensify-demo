52274f2eba189293d2977499c4a8fb1d873c9c55
Reid <touyalin@gmail.com>
2023-06-24 09:42:10 +0800

    basic setup

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import "normalize.css/normalize.css"
+import "./styles/style.scss"
+
+ReactDOM.render(<p>This is react app!</p>, document.getElementById("app"))
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+// entry -> output
+
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "none",
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devtool: "cheap-module-source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
+    compress: false,
+    port: 8080,
+  },
+  plugins: [
+    new webpack.ProvidePlugin({ 
+      process: "process/browser" 
+    }),
+  ],
+};
